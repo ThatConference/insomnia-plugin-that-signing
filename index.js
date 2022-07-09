2aea4bfbe0ac8d2007108793ac0709c62e9f7104
Brett Slaski <brett@thatconference.com>
2022-07-09 13:05:31 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const { security } = require('@thatconference/api');
+
+const thatSigHeader = 'that-request-signature';
+const placeholderText = '~headerdataplaceholder~'
+let signingKey;
+
+module.exports.templateTags = [{
+  name: 'thatrequestsigningplugin',
+  displayName: 'THAT Signature',
+  description: 'Generates a THAT Signature for request signing',
+  args: [
+    {
+      displayName: 'Signing Key',
+      type: 'string',
+      placeholder: 'THAT signing key',
+      description: 'Required value, secret signing key for signing request',
+    },
+  ],
+  run(context, key=''){
+    if (!key)
+      throw new Error('Signing Key is required to use THAT Signing plugin');
+    signingKey = key;
+
+    return placeholderText
+  },
+}];
+
+module.exports.requestHooks = [
+  (context) => {
+    let payload = context.request.getBody();
+    if (payload?.text) {
+      payload = JSON.parse(payload.text);
+    } else {
+      throw new Error('There is no request body to sign');
+    }
+    console.log('payload', payload)
+    const allHeaders = context.request.getHeaders();
+    const header = allHeaders.filter(h => h.value === placeholderText);
+    console.log('header', header);
+    if (header?.length > 1) 
+      throw Error('multiple headers found with placement text');
+    const headerName = header[0].name;
+    const thatSigning = security.requestSigning;
+    const requestSigning = thatSigning({ signingKey });
+    const signature = requestSigning.signRequest(payload);
+    console.log('signature', signature);
+    if (signature?.isOk !== true || !signature?.thatSig) {
+      throw new Error(`unable to sign request: ${signature?.message}`);
+    }
+    
+    context.request.setHeader(headerName, signature.thatSig);
+  }
+]
+ 
---
+++ readme.md
@@ -0,0 +1,28 @@
+# THAT Insomnia Signing Plugin
+
+Insomnia plugin for signing THAT Requests from Insomnia
+
+## Install
+
+This plugin is scoped to `@thatconference` and therefore doesn't appear on Insomnia's Plugin Hub.
+
+1. From Insomnia go to Preferences, Plugins tab
+1. Click `Reveal Plugins Folder` button
+1. This will open the plugins folder
+    a. MacOS: /Users/username/Library/Application Support/Insomnia/Plugins
+1. Open a terminal pointing to the directory
+1. run `npm install @thatconference/insomnia-plugin-that-signing`
+
+After refreshing the Plugins tab in Insomnia you should see this plugin listed.
+
+## Usage
+
+The signature needs to be sent as a Request Header. While setting up your header in Insomnia, for the header's value, press <ctrl+space> to get a list of `tags`. Use the `THAT Signature` tag. When you click on the tag a form for values is opened. The `Signing Key` field is required. Set your environment value for the signing key in this field.
+
+The `Live Preview` field will show something like, `~headerdataplaceholder~` when all is set. 
+
+### Making Requests
+
+Troubleshooting and throwing errors in Insomnia Plugins isn't great. If you discover that a signature value is not being sent try looking in the console (view > Toggle Dev Tools) and review the output from the plugin.
+
+When there is an error which was swallowed by Insomnia, the Timeline tag will be blank and no request would have been made.
