e0560bff56d191f32a19f1339f0f5526aa6d31b8
Brett Slaski <brett@thatconference.com>
2022-07-12 09:02:45 -0500

    shortcut out of request hook if no signing key

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
-const { security } = require('@thatconference/api');
+const { security } = require("@thatconference/api");
 
-const thatSigHeader = 'that-request-signature';
-const placeholderText = '~headerdataplaceholder~'
+const thatSigHeader = "that-request-signature";
+const placeholderText = "~headerdataplaceholder~";
 let signingKey;
 
-module.exports.templateTags = [{
-  name: 'thatrequestsigningplugin',
-  displayName: 'THAT Signature',
-  description: 'Generates a THAT Signature for request signing',
-  args: [
-    {
-      displayName: 'Signing Key',
-      type: 'string',
-      placeholder: 'THAT signing key',
-      description: 'Required value, secret signing key for signing request',
-    },
-  ],
-  run(context, key=''){
-    if (!key)
-      throw new Error('Signing Key is required to use THAT Signing plugin');
-    signingKey = key;
+module.exports.templateTags = [
+  {
+    name: "thatrequestsigningplugin",
+    displayName: "THAT Signature",
+    description: "Generates a THAT Signature for request signing",
+    args: [
+      {
+        displayName: "Signing Key",
+        type: "string",
+        placeholder: "THAT signing key",
+        description: "Required value, secret signing key for signing request",
+      },
+    ],
+    run(context, key = "") {
+      if (!key)
+        throw new Error("Signing Key is required to use THAT Signing plugin");
+      signingKey = key;
 
-    return placeholderText
+      return placeholderText;
+    },
   },
-}];
+];
 
 module.exports.requestHooks = [
   (context) => {
     let payload = context.request.getBody();
-    if (payload?.text) {
-      payload = JSON.parse(payload.text);
+    if (signingKey) {
+      if (payload?.text) {
+        payload = JSON.parse(payload.text);
+      } else {
+        throw new Error("There is no request body to sign");
+      }
     } else {
-      throw new Error('There is no request body to sign');
+      // we don't need this good to fire if the template and key isn't set
+      return;
     }
-    console.log('payload', payload)
+    console.log("payload", payload);
     const allHeaders = context.request.getHeaders();
-    const header = allHeaders.filter(h => h.value === placeholderText);
-    console.log('header', header);
-    if (header?.length > 1) 
-      throw Error('multiple headers found with placement text');
+    const header = allHeaders.filter((h) => h.value === placeholderText);
+    console.log("header", header);
+    if (header?.length > 1)
+      throw Error("multiple headers found with placement text");
     const headerName = header[0].name;
     const thatSigning = security.requestSigning;
     const requestSigning = thatSigning({ signingKey });
     const signature = requestSigning.signRequest(payload);
-    console.log('signature', signature);
+    console.log("signature", signature);
     if (signature?.isOk !== true || !signature?.thatSig) {
       throw new Error(`unable to sign request: ${signature?.message}`);
     }
-    
+
     context.request.setHeader(headerName, signature.thatSig);
-  }
-]
- 
+  },
+];
